a63f3c5c68c2956bef0104f33f4667d7dc933a2c
Marina Pinchuk <75565413+MarinettePinch@users.noreply.github.com>
2021-05-16 13:46:02 +0300

    Выполняет первое задание
    Написан скрипт для подсчёта и логирования количества категорий, их наименований и количества элементов в каждой категории

--- js/task-01.js
+++ js/task-01.js
@@ -13,3 +13,24 @@
 
 //Категория: Животные
 //Количество элементов: 4
+
+// 1. Написать функцию подсчёта длины массива:
+const countLengthArray = (array) => array.length;
+
+// 2. Написать функцию создания сообщения
+const creationMessage = (value) => `В списке ${value} категории.`;
+
+// 3. Получить массив объектов li.item
+const categories = document.querySelectorAll('.item');
+
+// 4. Вывести в консоль строку 'В списке n категории.'
+console.log(creationMessage(countLengthArray(categories)));
+
+// 5. Получить доступ к пунктам категорий, перебрав массив, не изменяя его
+
+[...categories].forEach((category) => {
+// 6. Вывести все заголовки
+    console.log('Категория:', category.firstElementChild.textContent);
+// 7. Получить все массивы - списки с li, осчититать их длину 
+    console.log('Количество элементов:', category.lastElementChild.children.length);
+});
