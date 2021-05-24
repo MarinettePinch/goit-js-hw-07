78e4a813cb28369310c73817eaede72db6728471
Marina Pinchuk <75565413+MarinettePinch@users.noreply.github.com>
2021-05-24 22:17:01 +0300

    Update task-05.js
    Получен доступ к элементам, написана функция для слушателя событий, добавлен слушатель событий

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,12 @@
-// // Напиши скрипт который, при наборе текста в инпуте 
+// // Напиши скрипт, который, при наборе текста в инпуте 
 // input#name-input (событие input), подставляет его текущее значение 
 // в span#name-output. Если инпут пустой, в спане должна отображаться 
 // строка 'незнакомец'.
+
+const outputEl = document.querySelector('#name-output');
+
+const inputEl = document.querySelector('#name-input');
+
+const updateOutputText = (event) => outputEl.textContent = event.currentTarget.value;
+
+inputEl.addEventListener('input', updateOutputText);
