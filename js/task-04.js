5419695ee8d95c0547266d958fcdf4c6af90f9dd
Marina Pinchuk <75565413+MarinettePinch@users.noreply.github.com>
2021-05-24 20:59:36 +0300

    Update task-04.js
    Создан объект для хранения значений счётчика и его функций.  Добавлены слушатели на кнопки увеличения и уменьшения значений счётчика, вызовы функций и обновление интерфейса

--- js/task-04.js
+++ js/task-04.js
@@ -6,3 +6,44 @@
 // Создай функции increment и decrement для увеличения и 
 //уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const counterEl = document.querySelector('#counter #value');
+
+const counterValue = counterEl.textContent;
+
+const counter = {
+    value: counterValue,
+    getValue() {
+        return this.value;
+    },
+    increment() {
+        this.value = Number(this.value)+1;
+     },
+    decrement() {
+        this.value = Number(this.value)-1;
+    },
+};
+
+
+const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
+
+const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
+
+
+const updateCounterValue = () => {
+    counterEl.textContent = counter.getValue();
+} 
+
+
+incrementBtn.addEventListener('click', counter.increment.bind(counter));
+
+incrementBtn.addEventListener('click', updateCounterValue.bind(counter));
+
+
+
+decrementBtn.addEventListener('click', counter.decrement.bind(counter));
+
+decrementBtn.addEventListener('click', updateCounterValue.bind(counter));
+
+
+
