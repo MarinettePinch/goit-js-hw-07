2bbb6d9dd43e6592c43abd0c7460d41501c22d3f
Marina Pinchuk <75565413+MarinettePinch@users.noreply.github.com>
2021-06-17 22:53:58 +0300

    Оптимизирует восьмое задание
    Убран слушатель событий с инпута, функция создания квадратов вынесена отдельно, но вызывается при событии клика на к кнопку "создать". Массив шаблонных строк создается с помощью конструктора.

--- js/task-08.js
+++ js/task-08.js
@@ -1,76 +1,73 @@
-// Напиши скрипт создания и очистки коллекции элементов. 
-// Пользователь вводит количество элементов в input и нажимает 
-// кнопку Создать, после чего рендерится коллекция. При нажатии 
-// на кнопку Очистить, коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает 1 
-// параметр amount - число. Функция создает столько div, 
-// сколько указано в amount и добавляет их в div#boxes.
-
-// Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого, должен быть 
-// шире и выше предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-
 const refs = {
-    input: document.querySelector('#controls input'),
-    renderBtn: document.querySelector('#controls button[data-action="render"]'),
-    destroyBtn: document.querySelector('#controls button[data-action="destroy"]'),
+    controls: document.querySelector('#controls'),
     boxes: document.querySelector('#boxes'),
-};
-
-let amount;
-
-const minNumber = Number(refs.input.getAttribute('min'));
-const maxNumber = Number(refs.input.getAttribute('max'));
-
-
-refs.input.addEventListener('input', onInput);
-
-function onInput (event){
-    const number = Number(event.currentTarget.value);
-    if (number%1 !== 0 || number < minNumber || number > maxNumber){
-        alert(`Ошибка! Введите число от ${minNumber} до ${maxNumber}!`);
-    } else {
-        amount = number;
-    }
-    refs.input.addEventListener('blur', onInputBlur);
-        
-    function onInputBlur (event){
-        event.currentTarget.value = '';
-    }
+    renderBtn: document.querySelector('#controls button[data-action="render"]'),
+    destroyBtn: document.querySelector('#controls button[data-action="destroy"]')
 }
 
-const createBoxes = () => {
+refs.controls.addEventListener('click', onButtonRenderClick);
+
+refs.controls.addEventListener('click', onButtonDestroyClick);
+
+function onButtonRenderClick(event) {
+    
+    if (event.target !== refs.renderBtn) return;
+
+    const num = Number(event.currentTarget.firstElementChild.value);
+
+    createBoxes(num);
+}
+
+let size = 30;
+
+function onButtonDestroyClick(event) {
+    
+    if (event.target !== refs.destroyBtn) return;
+
+    event.currentTarget.firstElementChild.value = '';
+
+    refs.boxes.innerHTML = '';
+
+    size = 30;
+
+}
+
+const rgbRandom = () => {
+    return Math.floor(Math.random() * 255);
+}
+
+
+class Div {
+
+  constructor(red, green, blue, size) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+    this.width = size;
+    this.height = size;
+  }
+
+  getSting() {
+    return `<div style="background-color: rgb(${this.red}, ${this.green}, ${this.blue}); width: ${this.width}px; height: ${this.height}px;"></div>`;
+  }
+
+}
+
+
+function createBoxes(amount) {
 
     const divArray = [];
+      
 
-    for(let i=0; i < amount; i+=1) {
+    for (let i = 0; i < amount; i += 1) {
         
-        const divEl = document.createElement('div');
         
-        divEl.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
-        divEl.style.width = (i * 10 + 30)+'px';
-        divEl.style.height = (i * 10 + 30)+'px';
+        const div = new Div(rgbRandom(), rgbRandom(), rgbRandom(), size);
         
-        divArray.push(divEl);
+        size += 10;
+        
+        divArray.push(div.getSting());
     }
 
-    refs.boxes.append(...divArray);
+    refs.boxes.insertAdjacentHTML('beforeend', divArray.join(''));
 }
-
-const destroyBoxes = () => {
-    refs.boxes.innerHTML = '';
-    amount = 0;
-}
-
-
-
-refs.renderBtn.addEventListener('click', createBoxes);
-
-refs.destroyBtn.addEventListener('click', destroyBoxes);
-
